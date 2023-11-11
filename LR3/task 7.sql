4d2f8dce14da64a5758efa4ebbd030ddeab3f792
Nigota <postaforjob@gmail.com>
2023-11-11 13:01:15 +0400

    Попытка 5.2

--- LR3/task 7.sql
+++ LR3/task 7.sql
@@ -4,11 +4,12 @@
 
 USE cd;
 
-SELECT DISTINCT CONCAT(m1.surname, ' ', m1.firstname) as Mem,
+SELECT CONCAT(m1.surname, ' ', m1.firstname) as Mem,
   (
-    SELECT DISTINCT CONCAT(m2.surname, ' ', m2.firstname)
+    SELECT CONCAT(m2.surname, ' ', m2.firstname)
       FROM members AS m2
       WHERE m2.memid = m1.recommendedby
   ) as Rec
   FROM members AS m1
+  WHERE m1.memid != 0
   ORDER BY Mem;
