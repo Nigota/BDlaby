49c1e68004b393676e8857db0736cff374b882e3
Nigota <postaforjob@gmail.com>
2023-11-20 19:36:34 +0400

    Попытка 1.2

--- LR5/task 17.sql
+++ LR5/task 17.sql
@@ -1,27 +1,23 @@
-/*
-Классифицируйте объекты на группы одинакового размера 
+/*Классифицируйте объекты на группы одинакового размера
 (высокие, средние и низкие в зависимости от их доходов). 
 Упорядочите по классификации и названию объекта.
-*/
+Примечание: используйте функцию деления на группы ntile.*/
 
 USE cd;
 
-WITH rank_table AS (
+WITH sum_table AS (
     SELECT b.facid, f.facility,
         SUM(
             IF(b.memid = 0, f.guestcost, f.membercost) * b.slots
-        ) - f.monthlymaintenance * COUNT(DISTINCT MONTH(b.starttime)) AS income,
-        DENSE_RANK() OVER(
-            ORDER BY SUM(IF(b.memid = 0, f.guestcost, f.membercost) * b.slots
-                    ) - f.monthlymaintenance * COUNT(DISTINCT MONTH(b.starttime)) DESC) AS ranking
+        ) - f.monthlymaintenance * COUNT(DISTINCT MONTH(b.starttime)) AS income
     FROM bookings AS b
         JOIN facilities AS f ON b.facid = f.facid
     GROUP BY b.facid
     )
-SELECT facility,
-  CASE
-    WHEN ranking <= maxi / 3 THEN "Высокий доход"
-    WHEN maxi / 3 < ranking AND ranking <= maxi / 3 * 2 THEN "Средний доход"
-    ELSE "Низкий доход"
-  END AS grp
-  FROM rank_table, (SELECT MAX(ranking) as maxi FROM rank_table) as tmp;
+SELECT facility, income,
+  CASE 
+    WHEN NTILE(3) OVER (ORDER BY income DESC) = 1 THEN 'Высокий доход'
+    WHEN NTILE(3) OVER (ORDER BY income DESC) = 2 THEN 'Средний доход'
+    WHEN NTILE(3) OVER (ORDER BY income DESC) = 3 THEN 'Низкий доход'
+  END AS incomegroup
+  FROM sum_table;
