39ce164b03e21102d09557cf97a8420bdfb2f986
Nigota <postaforjob@gmail.com>
2023-11-27 11:19:08 +0400

    Исправил

--- LR4/task 3.sql
+++ LR4/task 3.sql
@@ -5,7 +5,11 @@
 
 USE cd;
 
-INSERT facilities(facid, facility, membercost, guestcost, initialoutlay, monthlymaintenance)
-  SELECT 12, facility, membercost, guestcost, initialoutlay, monthlymaintenance 
+INSERT facilities
+  SELECT (SELECT MAX(facid) + 1 FROM facilities), facility, membercost, guestcost, initialoutlay, monthlymaintenance 
   FROM facilities 
-  WHERE facility LIKE 'spa';
+  WHERE facility LIKE 'spa'
+  LIMIT 1;
+
+SELECT * FROM facilities;
+
