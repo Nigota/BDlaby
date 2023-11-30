69fcdffaf212dfad3397371adc80b883d63c1499
Nigota <postaforjob@gmail.com>
2023-11-30 20:45:28 +0400

    Попытка 4

--- LR5/task 18.sql
+++ LR5/task 18.sql
@@ -6,68 +6,31 @@
 
 USE cd;
 
-SET @start_date = '2012-07-15', @end_date = '2012-08-31';
-
-START TRANSACTION;
-
-DELETE FROM bookings AS b
-  WHERE MONTH(b.starttime) = 8 AND DAY(b.starttime) BETWEEN 8 AND 10;
+SET @start_date = '2012-08-01', @end_date = '2012-08-31';
 
 WITH RECURSIVE dArray(dateDay) AS (
-    SELECT @start_date as dateDay
-    UNION ALL
-    SELECT DATE_ADD(dateDay, INTERVAL 1 day) FROM dArray WHERE dateDay < @end_date
-  )
-SELECT DATE_FORMAT(dr.dateDay, '%m %d') as dayMonth,
-    SUM(
+  SELECT @start_date as dateDay
+  UNION ALL
+  SELECT DATE_ADD(dateDay, INTERVAL 1 day)
+    FROM dArray
+    WHERE dateDay < @end_date
+)
+SELECT dateDay,
+    IFNULL(SUM(
       CASE
-        WHEN b.memid IS NULL THEN 0
-        WHEN b.memid = 0 THEN f.guestcost * b.slots
-        ELSE f.membercost * b.slots
-      END) AS income,
-    ROUND(AVG( 
-      SUM(
-        CASE
-          WHEN b.memid IS NULL THEN 0
-          WHEN b.memid = 0 THEN f.guestcost * b.slots
-          ELSE f.membercost * b.slots
-        END)  
-      ) OVER w) AS roll_avg
-  FROM dArray AS dr
-    LEFT JOIN bookings AS b ON DATE_FORMAT(b.starttime, '%m %d') = DATE_FORMAT(dr.dateDay, '%m %d')
-    LEFT JOIN facilities AS f ON b.facid = f.facid
-  GROUP BY dayMonth
-  WINDOW w AS (
-    ORDER BY DATE_FORMAT(dr.dateDay, '%m %d') ROWS BETWEEN 14 PRECEDING AND CURRENT ROW
-  )
-  ORDER BY dayMonth;
-ROLLBACK;
-
-WITH RECURSIVE dArray(dateDay) AS (
-    SELECT @start_date as dateDay
-    UNION ALL
-    SELECT DATE_ADD(dateDay, INTERVAL 1 day) FROM dArray WHERE dateDay < @end_date
-  )
-SELECT DATE_FORMAT(dr.dateDay, '%m %d') as dayMonth,
-    SUM(
-      CASE
-        WHEN b.memid IS NULL THEN 0
-        WHEN b.memid = 0 THEN f.guestcost * b.slots
-        ELSE f.membercost * b.slots
-      END) AS income,
-    ROUND(AVG( 
-      SUM(
-        CASE
-          WHEN b.memid IS NULL THEN 0
-          WHEN b.memid = 0 THEN f.guestcost * b.slots
-          ELSE f.membercost * b.slots
-        END)  
-      ) OVER w) AS roll_avg
-  FROM dArray AS dr
-    LEFT JOIN bookings AS b ON DATE_FORMAT(b.starttime, '%m %d') = DATE_FORMAT(dr.dateDay, '%m %d')
-    LEFT JOIN facilities AS f ON b.facid = f.facid
-  GROUP BY dayMonth
-  WINDOW w AS (
-    ORDER BY DATE_FORMAT(dr.dateDay, '%m %d') ROWS BETWEEN 14 PRECEDING AND CURRENT ROW
-  )
-  ORDER BY dayMonth;
+        WHEN (DATE(book.starttime) != dateDay) THEN 0
+        WHEN (book.memid = 0) THEN fac.guestcost * book.slots
+        ELSE fac.membercost * book.slots
+      END
+    ), 0) as income,
+    IFNULL(ROUND(SUM(
+        IF(
+            book.memid = 0,
+            fac.guestcost,
+            fac.membercost
+        ) * book.slots
+    ) / 15), 0) as avg_roll
+  FROM dArray
+    LEFT JOIN cd.bookings as book ON DATE_ADD(dateDay, INTERVAL -14 day) <= DATE(book.starttime) AND DATE(book.starttime) <= dateDay
+    LEFT JOIN cd.facilities as fac ON book.facid = fac.facid
+  GROUP BY dateDay;
