20741a4c46b18fd23d40eb71cb78c66d261f93d8
Nigota <postaforjob@gmail.com>
2023-12-23 12:48:02 +0400

    Доделал

--- LR8/task.sql
+++ LR8/task.sql
@@ -8,24 +8,33 @@
 USE Hospital;
 
 -- Текущее количество пациентов=-мужчин  и пациентов-женщин
-SELECT IF(gender = 0, 'Мужчин', 'Женщин') AS 'Пол', COUNT(*) AS 'Количество'
-  FROM patient
-  WHERE endTime IS NULL
-  GROUP BY gender;
+CREATE OR REPLACE VIEW cnt AS
+  SELECT IF(gender = 0, 'Мужчин', 'Женщин') AS 'Пол', COUNT(*) AS 'Количество'
+    FROM patient
+    WHERE endTime IS NULL
+    GROUP BY gender;
+
+SELECT * FROM cnt;
 
 
 -- Лечащиеся пациенты определенного отделения, например, Хирургии
-SELECT p.fullName, r.department
-  FROM patient as p
-    JOIN room as r ON r.roomID = p.roomID
-  WHERE endTime IS NULL
-  ORDER BY r.department;
+CREATE OR REPLACE VIEW cnt_patient AS
+  SELECT p.fullName, r.department
+    FROM patient as p
+      JOIN room as r ON r.roomID = p.roomID
+    WHERE endTime IS NULL
+    ORDER BY r.department;
+
+SELECT * FROM cnt_patient;
 
 -- Количество мест (отделение, общее кол-во мест (муж), 
 -- общее кол-во мест (жен) , свободн.места (муж), свободные места (жен).
-SELECT department AS 'Отделение', gender AS 'Пол', 
+CREATE OR REPLACE VIEW man AS
+  SELECT department AS 'Отделение', gender AS 'Пол', 
     SUM(bedCnt) AS 'Общее кол-во мест (по полу)',
     SUM(bedCnt) - SUM(bedOccupiedCnt) AS 'Свободн. мест (по полу)'
   FROM room
   GROUP BY department, gender 
   ORDER BY department;
+
+SELECT * FROM man;
