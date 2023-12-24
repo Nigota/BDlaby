890bbb35956dbb517bb5dafd919d4bb3840cc511
Nigota <postaforjob@gmail.com>
2023-12-24 23:23:43 +0400

    123

--- LR7/task 4.sql
+++ LR7/task 4.sql
@@ -6,183 +6,88 @@
 Сохраните расчет в виде csv или sql файла (например, используя временные таблицы). 
 */
 
-/*
-g - guest_cost
-m - member_cost
-x - cnt_guest
-y - cnt_member
-income = g * x + m * y - доход за месяц, g, x, m, y - нам известны
-income = g' * new_x + m' * new_y - доход за новый срок,
-new_x, new_y - количетсво посещений гостей и участников за новый период, по старым данным.
-(новый срок высчитываем по формуле w * days, w - доля в процентах, days - дней в месяце)
-Цена аренды для гостей и для участником можно связать соотношением m/g = k, 
-k - нам извесно (по старым данным), для простоты условимся, что это соотношение
-будет сохраняться и при новых ценах
-
-Тогда m'=kg', income = g' * new_x + k * g' * new_y => g' = income / (new_x + k * new_y)
-=> находим m'
-*/
-
 USE cd;
 
 DELIMITER //
 
--- получаем количество гостей за период
-DROP FUNCTION IF EXISTS get_guestcnt_inperiod //
-CREATE FUNCTION get_guestcnt_inperiod(facid INT, starttime TIMESTAMP, endtime TIMESTAMP)
-  RETURNS INT
-  READS SQL DATA
-  NOT DETERMINISTIC
-  BEGIN
-    DECLARE guestcnt INT;
-
-    SELECT COUNT(b.memid) INTO guestcnt
-      FROM bookings AS b
-      WHERE b.memid = 0 AND b.facid = facid AND
-      b.starttime BETWEEN starttime AND endtime;
-
-    RETURN guestcnt;
-  END //
-
--- получаем количество участников за период
-DROP FUNCTION IF EXISTS get_memcnt_inperiod //
-CREATE FUNCTION get_memcnt_inperiod(facid INT, starttime TIMESTAMP, endtime TIMESTAMP)
-  RETURNS INT
-  READS SQL DATA
-  NOT DETERMINISTIC
-  BEGIN
-    DECLARE memcnt INT;
-
-    SELECT COUNT(b.memid) INTO memcnt
-      FROM bookings AS b
-      WHERE b.memid != 0 AND b.facid = facid AND
-      b.starttime BETWEEN starttime AND endtime;
-
-    RETURN memcnt;
-  END //
-
--- получаем стоимость аренды для участников
-DROP FUNCTION IF EXISTS get_membercost //
-CREATE FUNCTION get_membercost(facid INT)
-  RETURNS DECIMAL(10, 0)
-  READS SQL DATA
-  NOT DETERMINISTIC
-  BEGIN
-    DECLARE memcost DECIMAL(10, 0);
-
-    SELECT f.membercost INTO memcost
-      FROM  facilities AS f
-      WHERE f.facid = facid;
-
-    RETURN memcost;
-  END //
-
--- получаем стоимость аренды для гостей
-DROP FUNCTION IF EXISTS get_guestcost //
-CREATE FUNCTION get_guestcost(facid INT)
-  RETURNS DECIMAL(10, 0)
-  READS SQL DATA
-  NOT DETERMINISTIC
-  BEGIN
-    DECLARE guestcost DECIMAL(10, 0);
-
-    SELECT f.guestcost INTO guestcost
-      FROM  facilities AS f
-      WHERE f.facid = facid;
-
-    RETURN guestcost;
-  END //
-
--- считаем доход за период
-DROP FUNCTION IF EXISTS income_of //
-CREATE FUNCTION income_of(facid INT, starttime TIMESTAMP, endtime TIMESTAMP)
-  RETURNS INT
-  READS SQL DATA
-  NOT DETERMINISTIC
-  BEGIN
-    DECLARE income INT;
-
-    SELECT SUM(p.payment) - f.monthlymaintenance * (
-        MONTH(endtime) - MONTH(starttime) + 1) INTO income
-      FROM payments AS p
-        JOIN bookings AS b ON b.bookid = p.bookid
-        JOIN facilities AS f ON b.facid = f.facid
-      WHERE facid = b.facid AND
-        b.starttime BETWEEN starttime AND endtime
-      GROUP BY b.facid;
-
-    RETURN income;
-  END //
-
--- считаем коэффициент соотношения цены гостей и участников
-DROP FUNCTION IF EXISTS get_k //
-CREATE FUNCTION get_k(facid INT, starttime TIMESTAMP, endtime TIMESTAMP)
-  RETURNS FLOAT
-  READS SQL DATA
-  NOT DETERMINISTIC
-  BEGIN
-    DECLARE guest_cost DECIMAL(10, 0);
-    DECLARE mem_cost DECIMAL(10, 0);
-
-    SELECT get_guestcost(facid) INTO guest_cost;
-    SELECT get_membercost(facid) INTO mem_cost;
-
-    RETURN mem_cost / guest_cost;
-  END//
-
--- получаем новую дату конца периода
-DROP FUNCTION IF EXISTS get_new_end //
-CREATE FUNCTION get_new_end(starttime TIMESTAMP, endtime TIMESTAMP, fraction FLOAT)
-  RETURNS TIMESTAMP
-  READS SQL DATA
-  NOT DETERMINISTIC
-  BEGIN
-    DECLARE new_end TIMESTAMP;
-
-    SELECT DATE_ADD(starttime, INTERVAL
-        ROUND(DATEDIFF(endtime, starttime) * fraction) DAY) INTO new_end;
-
-    RETURN new_end;
-  END//
-
--- считаем новую цену объекта
 DROP FUNCTION IF EXISTS increase_income_by //
-CREATE FUNCTION increase_income_by(facid INT, fraction FLOAT, starttime TIMESTAMP, endtime TIMESTAMP)
+CREATE FUNCTION increase_income_by(facid INT, fraction FLOAT, cur_time TIMESTAMP)
   RETURNS VARCHAR(50)
   READS SQL DATA
   NOT DETERMINISTIC
   BEGIN
-    DECLARE income DECIMAL(10, 0);
-    DECLARE new_end TIMESTAMP;
-    DECLARE new_guestcnt INT;
-    DECLARE new_memcnt INT;
-    DECLARE k FLOAT;
-    DECLARE g DECIMAL (10, 2);
-    DECLARE m DECIMAL (10, 2);
+    DECLARE income DECIMAL(10, 2);
+    DECLARE maintenance DECIMAL(10, 2);
+    DECLARE net_income DECIMAL(10, 2);
+    DECLARE cost DECIMAL(10, 2);
+    DECLARE beta DECIMAL(10, 2);
+    DECLARE result VARCHAR(50);
 
-    SELECT income_of(facid, starttime, endtime) INTO income;
-    SELECT get_new_end(starttime, endtime, fraction) INTO new_end;
-    SELECT get_guestcnt_inperiod(facid, starttime, new_end) INTO new_guestcnt;
-    SELECT get_memcnt_inperiod(facid, starttime, new_end) INTO new_memcnt;
-    SELECT get_k(facid, starttime, endtime) INTO k;
+    -- доход за все время эксплуатации объекта без учета обслуживания
+    SELECT SUM(p.payment) INTO income
+      FROM payments AS p
+        JOIN bookings AS b ON b.bookid = p.bookid
+        JOIN facilities AS f ON b.facid = f.facid
+      WHERE facid = b.facid AND
+        b.starttime <= cur_time
+      GROUP BY b.facid;
 
-    SELECT income / (new_guestcnt + k * new_memcnt) INTO g;
-    SELECT g * k INTO m;
+    IF income IS NULL THEN
+      BEGIN
+        SELECT CONCAT(f.guestcost, ';', f.membercost) INTO result
+        FROM facilities AS f
+        WHERE facid = f.facid;
+      RETURN result;
+      END;
+    END IF;
 
-    RETURN CONCAT(m, ';', g);
+
+    -- расходы на обслуживание за все время эксплуатации объекта (с точностью до дня)
+    SELECT f.monthlymaintenance * (MONTH(cur_time) - MONTH(MIN(b.starttime)) + 1) INTO maintenance
+      FROM facilities AS f
+        JOIN bookings AS b ON b.facid = f.facid
+      WHERE facid = b.facid
+      GROUP BY b.facid;
+
+    -- стоимость объекта
+    SELECT f.initialoutlay INTO cost
+      FROM facilities AS f
+      WHERE facid = f.facid;
+
+    -- очень важная переменная
+    SET net_income = (income - maintenance);
+    IF net_income <= 0 OR cost <= net_income THEN
+      BEGIN
+        SELECT CONCAT(f.guestcost, ';', f.membercost) INTO result
+        FROM facilities AS f
+        WHERE facid = f.facid;
+      RETURN result;
+      END;
+    END IF;
+
+    -- расчитываем коээфициент для стоимости аренды
+    SET beta = ((1 / fraction) * (cost - income + maintenance)
+                / cost * (1 - maintenance / income) + maintenance / income);
+
+    -- расчитываем новые сотимости аренды
+    IF beta > 0 THEN
+      SELECT CONCAT(f.guestcost * beta, ';', f.membercost * beta) INTO result
+        FROM facilities AS f
+        WHERE facid = f.facid;
+    ELSE
+      SELECT CONCAT(f.guestcost / (-1 * beta), ';', f.membercost / (-1 * beta)) INTO result
+        FROM facilities AS f
+        WHERE facid = f.facid;
+    END IF;
+
+    RETURN CONCAT(result);
   END //
 
 DELIMITER ;
 
--- задаем долю на которую изменяем срок окупаемости (в процентах)
-SET @start = CAST('2012-07-01' AS DATETIME);
-SET @end = CAST('2012-07-31-23:59:59' AS DATETIME);
-
-SELECT increase_income_by(4, 0.5, @start, @end)
+SELECT increase_income_by(0, 2, '2012-07-31-23:59:59')
 INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/output.csv' 
 FIELDS ENCLOSED BY '"' 
 TERMINATED BY ';' 
 ESCAPED BY '"' 
 LINES TERMINATED BY '\r\n';
-
-
--- LR7/test.sql
+++ LR7/test.sql
@@ -1,1 +1,11 @@
-mysql.server stop;
+USE cd;
+
+
+SELECT facility, IF(SUM(p.payment) IS NULL, 0, SUM(p.payment)) AS income
+      FROM payments AS p
+        JOIN bookings AS b ON b.bookid = p.bookid
+        JOIN facilities AS f ON b.facid = f.facid
+      WHERE b.starttime <= "2012-07-5-23:59:59"
+      GROUP BY facility;
+
+SELECT * FROM facilities;
